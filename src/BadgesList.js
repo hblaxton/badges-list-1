a7654d8460bf08e0f8ebaca2219351d0d80d5e09
Jessica Groblewski <jeg5933@psu.edu>
2023-03-28 12:36:26 -0400

    in class1

--- src/BadgesList.js
+++ src/BadgesList.js
@@ -11,11 +11,11 @@ export class BadgesList extends LitElement {
 
   static get styles() {
     return css`
-      :host {
+      /* :host {
         min-height: 100vh;
         display: flex;
         flex-direction: column;
-        align-items: center;
+        align-items: left;
         justify-content: flex-start;
         font-size: calc(10px + 2vmin);
         color: #1a2b42;
@@ -23,34 +23,59 @@ export class BadgesList extends LitElement {
         margin: 0 auto;
         text-align: center;
         background-color: var(--badges-list-background-color);
-      }
+      } */
 
-      main {
-        flex-grow: 1;
-      }
+    
 
-      .logo {
-        margin-top: 36px;
-        animation: app-logo-spin infinite 20s linear;
-      }
+      .wrapper{
+  background-color: lightgray;
+  color: white;
+  padding: 2px 2px 2px 20px;
+  text-align: center;
+  width: 400px;
+  height: auto;
+  border-radius: 5px;
+}
 
-      @keyframes app-logo-spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
+.badgeTitle {
+  background-color: lightblue;
+  color: white;
+  padding: 10px 8px;
+  text-align: left;
+  border-radius: 5px;
+  color: black;
+} 
+.badgeBody{
+  background-color: white;
+  color: black;
+  text-align: left;
+  padding: 2px 2px 2px 5px;
+  font-size: 20px;
+}
+.author{
+  background-color: white;
+  text-align: left;
+  color: black;
+  padding: 2px 2px 2px 5px;
+}
+.img{
+  text-align: right;
+background-color: white;
 
-      .app-footer {
-        font-size: calc(12px + 0.5vmin);
-        align-items: center;
-      }
+}
+.tblock {
+  color: black;
+  padding: 12px 20px 15px 20px;
+  background-color: grey;
+  margin: 25px;
+}
+.bblock {
+  color: black;
+  padding: 12px 20px 15px 20px;
+  background-color: grey;
+  margin: 25px;
+}
 
-      .app-footer a {
-        margin-left: 5px;
-      }
     `;
   }
 
@@ -61,30 +86,30 @@ export class BadgesList extends LitElement {
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.title}</h1>
+<h1 id = "heading" >Explore</h1>  
 
-        <p>Edit <code>src/BadgesList.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
+<div class="tblock">
+  <h3 id = "heading" >Explore our content in a self-guided manner. Want us to guide you through learning new skills? Try out Missions. Looking for other people with similar focus? Find them in Groups. Interested in viewing all the options within a certain subject area? You can do that with Topics</h3>
+  <input type="text" id="getme" placeholder="Search Content & Topics" />
+</div>
+<div class = bblock>
+  <h3 id = "bheading" > Looking for something brand spankin' new? Here are the most recently added badges! </h3>
 
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+
+<!-- <div> -->
+  <div class="wrapper">
+  
+    <div class="badgeTitle"> Technology & information </div>
+    <div class="badgeBody"> APA Style Citations: Introduction </div>
+  <div class="img">
+  <img src="https://badgesapp.psu.edu/uploads/badge/image/337/APA_Style.png" width="100" height="100">
+  
+  <div class="author"> Creator: Victoria Raish </div>
+  </div>
+</div>  </div>
+</div>
+
+  
     `;
   }
 }
