4fed8d1d7da7c0eda04f3c51d120c6b6b2e9f39e
btopro <btopro@outlook.com>
2023-04-03 19:39:47 -0400

    some event listening added to get search term

--- src/badges-list.js
+++ src/badges-list.js
@@ -2,8 +2,6 @@ import { LitElement, html, css } from 'lit';
 import "@lrnwebcomponents/simple-icon/simple-icon.js";
 import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
-
 export class BadgesList extends LitElement {
   static get properties() {
     return {
